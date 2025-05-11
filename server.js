c380760e3100477b656518fba02bf80bca661f40
Furkan Alkılıç <furkanalkilic@outlook.com>
2025-05-11 18:22:32 +0300

    update server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'source')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'source', 'template','index.html'));
+  res.sendFile(path.join(__dirname, 'docs','index.html'));
 });
 
 app.listen(PORT, () => {
